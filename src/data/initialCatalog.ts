9e9f60776da238387f055da59125dea5324c6fd2
kadwaolsztyn-afk <kadwaolsztyn@gmail.com>
2026-09-09 15:27:29 +0200

    Aktualizacja bazy (461 pozycji, 461 zdjęć, wersja 2026.03_ALL_v461_SYNC_20260909_1525)

--- src/data/initialCatalog.ts
+++ src/data/initialCatalog.ts
@@ -1,6 +1,6 @@
 import { DocumentRow, ImportedDocument } from '../types';
 
-export const CURRENT_DATABASE_VERSION = "2026.03_ALL_v461_SYNC_20260909_0914";
+export const CURRENT_DATABASE_VERSION = "2026.03_ALL_v461_SYNC_20260909_1525";
 
 export const INITIAL_461_CATALOG_ROWS: DocumentRow[] = [
   {
@@ -20334,7 +20334,7 @@ export const INITIAL_COMPREHENSIVE_CATALOG: ImportedDocument = {
   name: "sheet.html",
   fileType: "json",
   sizeFormatted: "320 KB",
-  importedAt: new Date("2026-09-09T07:14:16.433Z"),
+  importedAt: new Date("2026-09-09T13:25:53.662Z"),
   version: CURRENT_DATABASE_VERSION,
   totalRows: 461,
   brandsCount: 33,
